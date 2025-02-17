d71c75dfa7db3db7f6a4e21181d47e48abe834b2
HAlGhanim <imzaji98@gmail.com>
2025-02-17 22:29:25 +0300

    Done with Enums task

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+package userRole
 fun main() {
-    println("Hello World!")
+    val humoud = UserRole.ADMIN
+    val abdullah = UserRole.EDITOR
+    val ahmed = UserRole.VIEWER
+
+    humoud.access(3)
+    abdullah.access(2)
+    ahmed.access(2)
+    humoud.access(4)
+
+    println("-----")
+
+    for (role in UserRole.entries) { // UserRole.values() also works but intellij is recommending entries for iterating enums
+        println("${role.name} - ${role.accessLevel}")
+    }
 }
