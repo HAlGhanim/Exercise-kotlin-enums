c0ba204922f1f6bb4633494010253b2f9aa9bb4a
HAlGhanim <imzaji98@gmail.com>
2025-02-18 19:55:24 +0300

    Fixed access level and used when

--- src/Main.kt
+++ src/Main.kt
@@ -4,14 +4,22 @@ fun main() {
     val abdullah = UserRole.EDITOR
     val ahmed = UserRole.VIEWER
 
-    humoud.access(3)
-    abdullah.access(2)
-    ahmed.access(2)
-    humoud.access(4)
+    fun access(userRole: UserRole) {
+        when(userRole) {
+            UserRole.ADMIN -> println("You're an admin do whatever you want")
+            UserRole.EDITOR -> println("You're an editor you have limited privileges ")
+            UserRole.VIEWER -> println("You cannot alter anything.")
+        }
+    }
+
+    access(humoud)
+    access(abdullah)
+    access(ahmed)
 
     println("-----")
 
-    for (role in UserRole.entries) { // UserRole.values() also works but intellij is recommending entries for iterating enums
+    // UserRole.values() also works but intellij is recommending entries for iterating enums
+    for (role in UserRole.entries) {
         println("${role.name} - ${role.accessLevel}")
     }
 }
--- src/UserRole.kt
+++ src/UserRole.kt
@@ -2,19 +2,4 @@ package userRole
 
 enum class UserRole(val accessLevel: Int) {
     ADMIN(3), EDITOR(2), VIEWER(1);
-
-    fun access(levelRequired: Int): Boolean {
-        if (this.accessLevel >= levelRequired) {
-            println("You have access!")
-            return true
-        }
-        else if(levelRequired > 3){
-            println("Invalid access level.")
-            return false
-        }
-        else {
-            println("You don't have access.")
-            return false
-        }
-    }
 }
